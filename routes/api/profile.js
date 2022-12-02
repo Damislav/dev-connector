9c14a97e01d7ecb2c94363e2131c80e91cc3a94f
yournewgoodname <damislav@net.hr>
2022-12-02 20:40:27 +0100

    changed env

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const config = require("config");
 
 const { check, validationResult } = require("express-validator");
 
@@ -262,21 +261,21 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 // @route    GET api/profile/github/:username
 // @desc     Get user repos from Github
 // @access   Public
-router.get('/github/:username', async (req, res) => {
+router.get("/github/:username", async (req, res) => {
   try {
     const uri = encodeURI(
       `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
     );
     const headers = {
-      'user-agent': 'node.js',
-      Authorization: `token ${config.get('githubToken')}`
+      "user-agent": "node.js",
+      Authorization: `token ${process.env.githubToken}`,
     };
 
     const gitHubResponse = await axios.get(uri, { headers });
     return res.json(gitHubResponse.data);
   } catch (err) {
     console.error(err.message);
-    return res.status(404).json({ msg: 'No Github profile found' });
+    return res.status(404).json({ msg: "No Github profile found" });
   }
 });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const config = require("config");
+
 const { check, validationResult } = require("express-validator");
 const normalize = require("normalize-url");
 const User = require("../../models/User");
@@ -66,7 +66,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        process.env.JWT_SECRET,
         { expiresIn: "5 days" },
         (err, token) => {
           if (err) throw err;
